a3d051d691eace5eec32d8d39329da777c90897e
Tim Shimmin <timothy.shimmin.10+github@cnu.edu>
2015-05-13 18:16:22 -0400

    Much Cleaner.
    TODO:
    - Ask user to close Chrome.exe (or force close for them)
    - GUI?
    - Move to functions.
    - Error handling
    
    Cleaned up a lot.
    
    Changed save file date/time format to YearMonthDay from DayMonthYear.

--- ChromePassToCSV.py
+++ ChromePassToCSV.py
@@ -1,136 +1,62 @@
 __author__ = 'Tim Shimmin'
-from os import getenv
 import os
 from os.path import expanduser
 import sqlite3
 import win32crypt
-import csv
 import time
-import winshell
-
-
-
-# ## Example script functions
-# Create table
-# def createTestTable():
-#     c.execute('DROP TABLE if exists stocks')
-#     c.execute('''CREATE TABLE stocks
-#             (date text, trans text, symbol text, qty real, price real)''')
-#
-#     # Insert a row of data
-#     c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-#
-#     # Save (commit) the changes
-#     conn.commit()
-#
-#
-# # Read table
-# def readDb():
-#     c.execute('SELECT * FROM stocks WHERE qty=100')
-#     print(c.fetchone())
-# ## End Example script functions
-
 
 # ##### MAIN #####
-# ### modifications
-
-# expanduser attempt
-print()
-home = expanduser("~")
-# print(home)
-home = os.path.join(home, "AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data")
+# Find Chrome's password file, "Login Data".
+# Determines Windows User name with expanduser()
+## Assumes Windows 7, default installation directory.
+home = os.path.join(expanduser("~"), "AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data")
 print(home)
-print()
-# ### end expanduser attempt
 
 
-# getenv attempt
-# print('------')
-# chromeDir = os.path.join(os.getenv('APPDATA').rstrip("Roaming"), "Local\\Google\\Chrome\\User Data\\Default\\Login Data")
-# print(os.path.join(os.getenv('APPDATA').rstrip("Roaming"), "Local\\Google\\Chrome\\User Data\\Default\\Login Data"))
-# print('------')
-# # chromeDir = os.getenv('APPDATA').rstrip("Roaming")
-# # print(os.path.normpath(chromeDir))
-# # os.listdir(chromeDir)
-# # print(os.path.isfile(chromeDir)) # true
-# # print(os.path.realpath(chromeDir))
-# # chromeDir = os.path.realpath(chromeDir)
-# # print(chromeDir)
-
-# ### end getenv attempt
-
-# winshell attempt
-# print(winshell.application_data())
-# attempt = winshell.application_data()
-# print(winshell.)
-# ### end winshell attempt
-
-
-# chromePass = open('C:\\Users\\Kat\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data')
-# chromePass = open('C:\\Users\\Kat\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data')
-# conn1 = sqlite3.connect(win32api.GetEnvironmentVariable("APPDATA") + "\..\Local\Google\Chrome\User Data\Default\Login Data")
-# oldTODO: path is found (expanduser attempt working), but cannot access because it //is a hidden file?// WAS CURRENTLY IN USE, BECAUSE CHROME.EXE WAS OPEN. -->prev iteration:*** not helpful for any other computer. Assumes 1. Windows machine and 2. User name of "Kat"; perhaps "%LOCALAPPDATA%\Google\Chrome\User Data\Default\"
 # TODO: Require user to exit from Chrome.exe before use -> http://stackoverflow.com/a/10457565
-
-
-conn1 = sqlite3.connect(home)  # for expanduser attempt
-
-# conn1 = sqlite3.connect(chromeDir) # for getenv attempt
-
-# conn1 = sqlite3.connect(getenv("APPDATA") + "\\..\\Google\\Chrome\\User Data\\Default\\Login Data")
-# conn1 = sqlite3.connect("C:\\Users\\Kat\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data")
-cursor = conn1.cursor()
-
-# csv attempt
-# csvFile = open('Chrome Passwords' + time.strftime("%d-%m-%Y_%H-%M-%S") + '.csv', 'w', newline='')  # , encoding='utf-8')
-# fileWriter = csv.writer(csvFile)  # , dialect=csv.excel  # TODO: delimiter or lineterminator? http://automatetheboringstuff.com/chapter14/
-# ## end csv attempt
-
-txtFile = open('Chrome Passwords' + time.strftime("%d-%m-%Y_%H-%M-%S") + '.csv', 'w')
-# fileWriter =
-
-
-
+# TODO: Check if running before and after this. Before, then pose question; After, then wait some seconds?
 # Before accessing Chrome file while in use, exit from all instances of Chrome.exe
-# print("Would you like me to force close Chrome? Yes/No") # TODO: Check if running before and after this. Before, then pose question; After, then wait some seconds?
+# print("Would you like me to force close Chrome? Yes/No")
 # answer = input()
 # if answer == "Yes" or answer == "yes":
 os.system("taskkill /f /im chrome.exe >nul")
     # else:
     #     throw exception
 
+
+# Opens writable file to save passwords to. Labeled with date and timestamp.
+## Saving with extension .csv is readable by OpenOffice4
+txtFile = open('Chrome Passwords ' + time.strftime("%Y-%m-%d_%H-%M-%S") + '.csv', 'w')
+
 try:
+    # Opens sqlite3 interface. Create connection to "Login Data" sqlite3 database.
+    conn = sqlite3.connect(home)
+    cursor = conn.cursor()      # Used to traverse records from the result set
+
+    # Execution of SQL statement. Selects website, username, and password values from Chrome's pwd file
     cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+
+    # For each row of values, write decrypted row to txtFile
     for result in cursor.fetchall():
         try:
-            password = win32crypt.CryptUnprotectData(result[2],None,None,None,0)[1]
+            # Removes Windows encryption on password_value for current user.
+            password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
         except:
             print(result + "Decrypt Failed")
-
-        # TODO: Instead of print to console, save to CSV file. Perhaps in order (once) required by Firefox.
-        # print('Site: ' + result[0])
-        # print('Username: ' + result[1])
-        # print("Password: " + password.decode('utf-8'))
-        # fileWriter.writeRow([result[0], result[1]])  # , password.decode('utf-8') # csv attempt
-        txtFile.write(result[0] + "," + result[1] + "," + password.decode('utf-8') + "\n")      #  password.decode('utf-8'),
-    # print(cursor.fetchone())
+        # Writes row with values separated by commas. Row separated with \n.
+        # Decodes password from binary (bytes) string to utf-8.
+        txtFile.write(result[0] + "," + result[1] + "," + password.decode('utf-8') + "\n")
 except:
-    cursor.close()
-    conn1.close()
-    # csvFile.close()
+    conn.close()
     txtFile.close()
-# cursor.close()
-# conn1.close()
-
-# print(chromePass.read())
-# ############# end mods
+    # cursor.close()        # Closes with conn.close()
+    # csvFile.close()
 
 
-
-
+# TODO: Make it *this* readable.
 # ORIGINAL EXAMPLE PROGRAM
-# conn = sqlite3.connect('example.db')
 #
+# conn = sqlite3.connect('example.db')
 # c = conn.cursor()
 #
 # # Create test table
